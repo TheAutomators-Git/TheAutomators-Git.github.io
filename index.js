c3da7ea5feab5724130f34e5a30655363070ba5a
EznXadee <eznzaidi@gmail.com>
2024-07-06 23:09:57 +0500

    Automaters Website Updated Hero Section And About Section

---
+++ index.js
@@ -0,0 +1,33 @@
+// let hero = document.getElementById("construction-section")
+// let data = [
+//     {
+//         url:"https://media.istockphoto.com/id/1367967285/photo/automation-software-technology-process-system-business-concept.jpg?s=612x612&w=0&k=20&c=VnRZHYgIfInFhaShiStMroW7PnVyvAC4C9SodjBf4ns=",
+//         title:"Automation",
+//         description:"Loren Ipsum Loren Ipsum Loren Ipsum Loren Ipsum Loren Ipsum Loren Ipsum Loren IpsumLoren Ipsum Loren IpsumLoren Ipsum "
+//     },
+//     {
+//         url:"https://images.unsplash.com/photo-1674027444485-cec3da58eef4?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//         title:"Networking",
+//         description:"Loren Ipsum Loren Ipsum Loren Ipsum Loren Ipsum Loren Ipsum Loren Ipsum Loren IpsumLoren Ipsum Loren IpsumLoren Ipsum"
+//     },
+//     {
+//         url:"https://www.shutterstock.com/image-vector/education-science-concept-banner-chemistry-600nw-772020961.jpg",
+//         title:"Research",
+//         description:"Loren Ipsum Loren Ipsum Loren Ipsum Loren Ipsum Loren Ipsum Loren Ipsum Loren IpsumLoren Ipsum Loren IpsumLoren Ipsum"
+//     }
+// ]
+// let htmlContent = '<div class="Hero">';
+// data.forEach(item => {
+//     htmlContent += `
+//         <div class="box-container">
+//             <h1>${item.title}</h1>
+//             <div class="box">
+//                 <img src=${item.url}  width= "350px" height= "270px" />
+//                 <p>${item.description}</p>
+//             </div>    
+//         </div>
+//     `;
+// });
+// htmlContent += '</div>';
+
+// hero.innerHTML = htmlContent;
